ecbb4a2b477b15ab6b83eec53831f8d5df6022a7
Afton Downey and James Turello <aaike001@gmail.com>
2016-01-14 17:19:44 -0800

    add correct jQuery code to fix scope issue with looping.html not being able to find get_radio_value() function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $("form#user-input").submit(function(event){
+
+  $("form#user-input").submit(function(){
     var operator = $("select#operator").val();
     var operand1 = parseInt($(input1).val());
     var operand2 = parseInt($(input2).val());
@@ -7,10 +8,20 @@ $(document).ready(function() {
     alert(math_it_up(operand1, operand2, operator));
   });
 
-function math_it_up(opnd1, opnd2, optr){
+  function math_it_up(opnd1, opnd2, optr){
 
-  if (optr = '+'){
-    return opnd1 + opnd2;
-  }
-};
+    if (optr = '+'){
+      return opnd1 + opnd2;
+    }
+  };
+
+  $("form#genre").submit(function get_radio_value(event) {
+  for (var i=0; i < document.orderform.music.length; i++) {
+     if (document.orderform.music[i].checked) {
+        var rad_val = document.orderform.music[i].value;
+        alert(rad_val);
+        }
+     }
+     event.preventDefault();
+  });
 });
